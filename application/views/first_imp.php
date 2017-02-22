e5fb895bbf01f1cbf42e9c29a4620f4bdaf1bfac
unknown <ram@ram.com>
2017-02-22 10:37:34 +0530

    first time commit

---
+++ application/views/first_imp.php
@@ -0,0 +1,7 @@
+<html>
+<body align='center' padding='10'>
+<p> Welcome to CodeIgniter </p> <br>
+<a href='user/login'>Click Here for login </a><br><br>
+<a href='user/signup'>Click Here for Signup</a><br>
+</body> 
+</html>
